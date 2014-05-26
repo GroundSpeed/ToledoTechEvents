52432ebb65d7b7a34f670af45465fe6c52865af6
donmiller <dmiller@jadr.biz>
2014-05-25 21:25:31 -0400

    Added navigation controller for navbar on events screen

---
+++ Toledo Tech/EventNavigationController.h
@@ -0,0 +1,14 @@
+//
+//  EventNavigationController.h
+//  Toledo Tech
+//
+//  Created by Don Miller on 5/25/14.
+//  Copyright (c) 2014 GroundSpeed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Constants.h"
+
+@interface EventNavigationController : UINavigationController
+
+@end
